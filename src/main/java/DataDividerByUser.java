509b74cef409fb41bde61944a4bde028a8358ded
Shenghong Wang <shenghong1123@outlook.com>
2017-08-18 10:31:12 -0700

    divide data by user

--- src/main/java/DataDividerByUser.java
+++ src/main/java/DataDividerByUser.java
@@ -10,6 +10,7 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 public class DataDividerByUser {
 	public static class DataDividerMapper extends Mapper<LongWritable, Text, IntWritable, Text> {
@@ -19,7 +20,14 @@ public class DataDividerByUser {
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
 			//input user,movie,rating
+            //output user \t movie:rating
 			//divide data by user
+			String[] line = value.toString().trim().split(",");
+			int user = Integer.parseInt(line[0]);
+			String movie = line[1];
+			String rating = line[2];
+			context.write(new IntWritable(user), new Text(movie + ":" + rating));
+
 		}
 	}
 
@@ -30,6 +38,13 @@ public class DataDividerByUser {
 				throws IOException, InterruptedException {
 
 			//merge data for one user
+            StringBuilder sb = new StringBuilder();
+            Iterator it = values.iterator();
+            while (it.hasNext()) {
+                sb.append(",").append(it.next());
+            }
+            // key = user, value = movie1:rating, movie2:rating...
+            context.write(key, new Text(sb.toString().replaceFirst(",", "")));
 		}
 	}
 
